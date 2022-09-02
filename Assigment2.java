2e80b2537e47e205b6440da8859556dbd132e406
sourav8554 <110466225+sourav8554@users.noreply.github.com>
2022-09-02 15:00:14 +0530

    Add files via upload

---
+++ Assigment2.java
@@ -0,0 +1,15 @@
+package operators1;
+
+
+
+public class Assigment2 {
+	void calculateArea() {
+		System.out.println("\" length*breadth\"");
+	}
+}
+class AreaCalculator{
+	public static void main(String[] args) {
+		Assigment2 rec=new Assigment2();
+		rec.calculateArea();
+	}
+}
